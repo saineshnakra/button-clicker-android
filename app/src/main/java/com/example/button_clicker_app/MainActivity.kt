5c85e4fc7fa929e53b834b655cffc74606cf0458
Sainesh Nakra <30744887+saineshnakra@users.noreply.github.com>
2024-05-06 18:28:57 +0530

    initial-layout

--- app/src/main/java/com/example/button_clicker_app/MainActivity.kt
+++ app/src/main/java/com/example/button_clicker_app/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.button_clicker_app
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -14,10 +17,16 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.button_clicker_app.ui.theme.ButtonclickerappTheme
 
 class MainActivity : ComponentActivity() {
+    private var userInput : EditText? = null
+    private var button : Button? = null
+    private var textView : TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main);
+        userInput = findViewById(R.id.editTextTextMultiLine)
+        button = findViewById(R.id.button)
+        textView = findViewById(R.id.textView)
 
     }
 }
